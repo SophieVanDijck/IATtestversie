178fa2de9f23eee47667b121f0c667ddc9867242
SophieVanDijck <119676242+SophieVanDijck@users.noreply.github.com>
2022-12-05 20:25:02 +0100

    Update zonderattrib.js

--- zonderattrib.js
+++ zonderattrib.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://sophievandijck.github.io/IATtestversie/IAtphysioperspective.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
